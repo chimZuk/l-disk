ce0d9463885e0e7124c58ab6069ae61aae49fe11
Dzmitry Kuzmitch <dzmitry.kuzmitch@gmail.com>
2020-12-01 16:25:52 +0000

    changes to the main config

--- server/models/secret_data.js
+++ server/models/secret_data.js
@@ -1,6 +1,6 @@
-const uri = "mongodb://chimzuk:!4115Mama@photos.chimZuk.com:27017/drive?authSource=admin&readPreference=primary&appname=MongoDB%20Compass&ssl=false";
+const uri = "mongodb://admin:!4115Mama@91.149.139.46:27017/drive?authSource=admin&readPreference=primary&appname=MongoDB%20Compass&ssl=false";
 const secret = "EBATTEBYA";
-const library = "D:\\Photos"
+const library = "/home/ubuntu/storage"
 
 module.exports = {
     secret: secret,
